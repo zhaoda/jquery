ee8754d3df3b7b480a83bcae760bee979b844ab7
jaubourg <j@ubourg.net>
2011-03-14 15:59:11 +0100

    Adds always and chain methods to deferred and promises.

--- src/deferred.js
+++ src/deferred.js
@@ -1,7 +1,7 @@
 (function( jQuery ) {
 
 var // Promise methods
-	promiseMethods = "then done fail isResolved isRejected promise".split( " " ),
+	promiseMethods = "done fail isResolved isRejected promise then always chain".split( " " ),
 	// Static reference to slice
 	sliceDeferred = [].slice;
 
@@ -100,10 +100,27 @@ jQuery.extend({
 				deferred.done( doneCallbacks ).fail( failCallbacks );
 				return this;
 			},
+			always: function() {
+				return deferred.done.apply( deferred, arguments ).fail.apply( this, arguments );
+			},
 			fail: failDeferred.done,
 			rejectWith: failDeferred.resolveWith,
 			reject: failDeferred.resolve,
 			isRejected: failDeferred.isResolved,
+			// Chain
+			chain: function( fn ) {
+				var newDefer = jQuery.Deferred(),
+					returned;
+				deferred.done(function() {
+					returned = fn.apply( this, arguments );
+					if ( jQuery.isFunction( returned.promise ) ) {
+						returned.promise().then( newDefer.resolve, newDefer.reject );
+					} else {
+						newDefer.resolve( returned );
+					}
+				}).fail( newDefer.reject );
+				return newDefer.promise();
+			},
 			// Get a promise for this deferred
 			// If obj is provided, the promise aspect is added to the object
 			promise: function( obj ) {
